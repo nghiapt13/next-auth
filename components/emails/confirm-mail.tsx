b5b7be3cea48db71b3b7c72e407341a13f2d291a
nghiapt13 <fb.trungnghia13@gmail.com>
2024-01-08 23:55:53 +0700

    fix

--- components/emails/confirm-mail.tsx
+++ components/emails/confirm-mail.tsx
@@ -89,7 +89,7 @@ export const ConfirmMail = ({
                             located in{" "}
                             <span className="text-black">{inviteFromLocation}</span>. If you
                             were not expecting this invitation, you can ignore this email. If
-                            you are concerned about your account's safety, please reply to
+                            you are concerned about your account&apos;s safety, please reply to
                             this email to get in touch with us.
                         </Text>
                     </Container>
